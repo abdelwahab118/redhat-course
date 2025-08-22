50d7658ceb821ced0f169611471ec2d9ccdcc6b1
Abdelwahab Ehab <Abdelwahab.ehab11@gmail.com>
2025-08-22 19:58:08 +0300

    Initial Test Openshift

---
+++ src/test/java/com/alahli/demoopenshift/DemoOpenShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alahli.demoopenshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOpenShiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
